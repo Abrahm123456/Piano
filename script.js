3908574e44db264b2797e01eacb3cd42bd49b3fc
Abrahm123456 <aaijqy@outlook.com>
2026-02-28 16:15:37 +0000

    try to make audio work

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const keys = document.querySelectorAll('.piano-keys, piano-black-keys');
-const sound = new Audio('1.mp3');
+const keys = document.querySelectorAll('.piano-keys, .piano-black-keys');
+const sound = new Audio('{note}.mp3');
 
 keys.forEach((key) => {
   key.addEventListener('click',(e) =>{
     const keyclicked = e.target.dataset.note;
-    sound.src = `${keyclicked}.mp3`;
+    console.log(keyclicked);
+    sound.src = `./Audios/${keyclicked}.mp3`;
+    sound.currentTime = 0;
     sound.play ();
   })})
