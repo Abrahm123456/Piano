858205cad037e021217c80a84674194319346cc8
Abrahm123456 <aaijqy@outlook.com>
2026-04-11 15:54:03 +0000

    test csv

--- script.js
+++ script.js
@@ -4,7 +4,46 @@ keys.forEach((key) => {
   key.addEventListener('click',(e) =>{
     const keyclicked = e.target.dataset.note;
     console.log(keyclicked);
-    sound.src = `Audios/${keyclicked}.mp3`;
-    sound.currentTime = 0;
-    sound.play ();
+    // sound.src = `Audios/${keyclicked}.mp3`;
+    // sound.currentTime = 0;
+    // sound.play ();
+      playSong(testInput);
   })})
+  async function playSong(csvFile) {
+    const lines = csvFile.trim().split('\n');
+    for (const line of lines) {
+      const [note, duration] = line.split(',').map(item => item.trim());
+      sound.src = `Audios/${note}.mp3`;
+      sound.currentTime = 0;
+      sound.play();
+      await new Promise(resolve => setTimeout(resolve, parseInt(duration)));
+    }
+  }
+  const testInput = `
+5,3
+4,1
+3,1
+4,1
+5,1
+5,1
+5,2
+4,1
+4,1
+4,2
+5,1
+6,1
+6,2
+5,3
+4,1
+3,1
+4,1
+5,1
+5,1
+5,2
+4,1
+4,1
+4,1
+5,1
+4,1
+3,3`;
+  playSong(testInput);
