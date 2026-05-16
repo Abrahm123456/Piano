67c361b0131ab625b6d118a537ed9cc27009f0ce
Abrahm123456 <aaijqy@outlook.com>
2026-05-16 15:53:53 +0000

    BIG FIX

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
+letisplaying = false;
 const song_dropdown = document.getElementById('songs');
 const play_button = document.getElementById('play-button');
 const keys = document.querySelectorAll('.piano-keys, .piano-black-keys');
-const sound = new Audio('{note}.mp3');
 keys.forEach((key) => {
   key.addEventListener('click',(e) =>{
     const keyclicked = e.target.dataset.note;
     console.log(keyclicked);
-    // sound.src = `Audios/${keyclicked}.mp3`;
-    // sound.currentTime = 0;
-    // sound.play ();
-      playSong(testInput);
+    const sound = new Audio('{note}.mp3');
+     sound.src = `Audios/${keyclicked}.mp3`;
+     sound.currentTime = 0;
+     sound.play ();
   })})
   async function playSong(csvFile) {
     const lines = csvFile.trim().split('\n');
@@ -22,7 +22,25 @@ keys.forEach((key) => {
       await new Promise(resolve => setTimeout(resolve, parseInt(duration) * 300));
     }
   }
-  fetch('mary_had_a_little_lamb.csv')
+play_button.addEventListener('click', () => {
+  if (isplaying) {
+    isplaying = false;
+    play_button.textContent = 'Play';
+    return;
+  }
+
+    const selectedSong = song_dropdown.value;
+    if (selectedSong === 'freeplay') {
+      return;
+    }
+
+    try {
+      isplaying = true;
+      play_button.textContent = 'Stop';
+      const response = await fetch(`Songs/${selectedSong}.csv`);
+    }
+
+  fetch(`Songs/${selectedSong}.csv`)
     .then(response => response.text())
     .then(data => {
       const testInput = data
